280a3f01242b2064b822f5a825ebd21aecafd1a1
Peter <peter.kalavsky@accenture.com>
2021-04-03 20:53:16 +0200

    Using jinja template system

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 from flask import Flask 
 from flask import render_template
+from database import articles
 
 flask_app = Flask(__name__)
 
 @flask_app.route("/")
 def view_welcome_page():
-	return render_template("welcome_page.html")
-
+	return render_template("welcome_page.jinja")
 
 @flask_app.route("/about/")
 def view_about():
 	return render_template("about.jinja")
+
+@flask_app.route("/admin/")
+def view_admin():
+	return render_template("admin.jinja")
+
+@flask_app.route("/articles/")
+def view_articles():
+	return render_template("articles.jinja", articles=articles.items())
+
+@flask_app.route("/articles/<int:art_id>")
+def view_article(art_id):
+	article = articles.get(art_id)
+	if article:
+		return render_template("article.jinja", article=article)
+	return render_template("article_not_found.jinja", art_id=art_id)
+
+
